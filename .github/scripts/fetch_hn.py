f4fbf31fa8aa1178bd3ce304cd143868ab73a806
Mriyam Tamuli <mbtamuli@gmail.com>
2025-06-21 20:02:26 +0530

    Output formatting
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- .github/scripts/fetch_hn.py
+++ .github/scripts/fetch_hn.py
@@ -4,13 +4,14 @@ import requests
 def fetch_top_hackernews(n=3):
     top_stories = requests.get('https://hacker-news.firebaseio.com/v0/topstories.json').json()
     news = []
-    for story_id in top_stories[:n]:
+    for idx, story_id in enumerate(top_stories[:n], 1):
         story = requests.get(f'https://hacker-news.firebaseio.com/v0/item/{story_id}.json').json()
-        news.append(f"{story['title']} - {story.get('url', 'No URL')}\n")
+        title = story['title']
+        url = story.get('url', f'https://news.ycombinator.com/item?id={story_id}')
+        news.append(f"**{idx}. [{title}]({url})**")
     return news
 
 if __name__ == "__main__":
     top_news = fetch_top_hackernews()
     with open('hn_top3.txt', 'w') as f:
-        for item in top_news:
-            f.write(item)
+        f.write('\n'.join(top_news))
